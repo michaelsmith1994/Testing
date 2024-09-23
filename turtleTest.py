f1f43412144f8a508b1500c8768ab1e50e5886ee
michaelsmith1994 <michaelsmith1994@yahoo.com>
2024-09-22 21:16:31 -0500

    first commit

---
+++ turtleTest.py
@@ -0,0 +1,10 @@
+import turtle as turt
+
+# Create a turtle object
+t = turt.Turtle()
+
+# Write "Hello World"
+t.write("Hi Derek, get back to work.", align="center", font=("Arial", 24, "normal"))
+
+# Keep the window open until manually closed
+turt.done()
